bde8fcc02f8b3c687f7634b7e4a724a90bc06fa7
VictorAcarvalho <carvalho.victorfa@gmail.com>
2021-07-25 22:10:32 -0300

    implementa o localStorage dos produtos

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const userEmail = document.getElementById('user-email');
 const userName = document.getElementById('user-name');
 const btnSetItem = document.getElementById('btnSetItem');
+const product = document.getElementsByClassName('productDescription').value;
+
 
 setItemLocal=()=>{
           localStorage.setItem('name',`${userName.value}`);
         localStorage.setItem('email',`${userEmail.value}`);
     }
+
+setCartLocal=()=>{
+  localStorage.setItem('product',`${product.value}`);
+
+}
